ad2080fb7d381374a4bec1a76a391f0ad9d19776
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-13 03:51:45 +0200

    orgin component virement

---
+++ app/Http/Livewire/Virement/OrginComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Virement;
+
+use Livewire\Component;
+
+class OrginComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.virement.orgin-component');
+    }
+}
--- resources/views/operations/virement.blade.php
+++ resources/views/operations/virement.blade.php
@@ -5,6 +5,11 @@
 <div class="row">
 	@include('operations.side')
 	<div class="col-md-10 col-sm-12">
+		<div class="row">
+			<div class="col-md-4">
+				@livewire("virement.orgin-component")
+			</div>
+		</div>
 	</div>
 </div>
 
